ed579bd6049e29aef5dd7bfaf3fd2b2a6ba2a2ab
MAJID DASHTI <mdasht65@gmail.com>
2025-06-06 21:16:09 +0330

    اضافه شدن مسیر /shops

--- app.py
+++ app.py
@@ -38,9 +38,6 @@ def register():
 
     return f"✅ حجره با نام {shop_name} ثبت شد!"
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
 
 
 @app.route('/shops')
@@ -52,4 +49,18 @@ def show_shops():
         data = []
 
     return render_template("shops.html", shops=data)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
+
+
 
